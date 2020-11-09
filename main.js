dad2ca2eb4f40edbfce6c9e136f182d7dc425d33
Sabrina <sabrinacunsolo85@gmail.com>
2020-11-09 15:11:18 +0100

    inizio esercizio

---
+++ main.js
@@ -0,0 +1,18 @@
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+            // creo array di img
+            immagini: [
+                'https://desenio.it/bilder/artiklar/zoom/8386_2.jpg?imgwidth=435&qt=',
+                'https://desenio.it/bilder/artiklar/zoom/12897_2.jpg?imgwidth=435&qt=Illustrazione%20in%20rosa',
+                'https://desenio.it/bilder/artiklar/zoom/8233_2.jpg?imgwidth=435&qt=',
+                'https://desenio.it/bilder/artiklar/zoom/12326_2.jpg?imgwidth=435&qt=',
+                'https://desenio.it/bilder/artiklar/zoom/2920_2.jpg?imgwidth=435&qt='
+            ]
+        },
+        methods: {
+
+        }
+    }
+);
